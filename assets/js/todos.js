02305a42164329bcdcac6dd70fa36bd1b19a529f
Lukas <lukaszpawelec89@gmail.com>
2017-11-18 17:23:35 +0100

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -25,7 +25,7 @@ $(this).toggleClass("completed");
 //click on X to delete a todo
 
 $("ul").on("click", "span", function(event){
-    $(this).parent().fadeOut(500, function(){
+    $(this).parent().slideToggle(200, function(){
         $(this).remove();
     });
     event.stopPropagation();
@@ -39,12 +39,12 @@ $("input[type='text']").keypress(function(event){
        var todoText = $(this).val();
        $(this).val("");
        // creating new li and add to ul
-       $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span>" + todoText + "</li>")
+       $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span>" + todoText + "</li>");
     };
 });
 
 $("input[type='text']").hide();
 
 $(".fa-plus-circle").click(function(){
-    $("input[type='text']").fadeToggle();
+    $("input[type='text']").slideToggle(50);
 });

42dd434a89ff90c8929638287e5e1a78f3433a68
Lukas <lukaszpawelec89@gmail.com>
2017-11-18 15:23:09 +0100

    Add files via upload

---
+++ assets/js/todos.js
@@ -0,0 +1,50 @@
+// Checking off specific todos by clicking
+// $("li").click(function(){
+//     if($(this).css("color") === "rgb(128, 128, 128)"){
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         });
+//     }
+//     else{
+//         $(this).css({
+//             color: "gray",
+//             textDecoration: "line-through"
+//         });
+//     }
+    
+
+// });
+
+// Checking off todos
+
+$("ul").on("click", "li", function(){
+$(this).toggleClass("completed");
+});
+
+//click on X to delete a todo
+
+$("ul").on("click", "span", function(event){
+    $(this).parent().fadeOut(500, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
+});
+
+//add a new todo
+
+$("input[type='text']").keypress(function(event){
+    if(event.which === 13){
+        // grabbing text from text input
+       var todoText = $(this).val();
+       $(this).val("");
+       // creating new li and add to ul
+       $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span> " + todoText + "</li>")
+    };
+});
+
+$("input[type='text']").hide();
+
+$(".fa-plus-circle").click(function(){
+    $("input[type='text']").fadeToggle();
+});

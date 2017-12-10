7e3f4a39f5fb634236bd5692bec04c3c6bf989b3
Lukas <lukaszpawelec89@gmail.com>
2017-12-10 13:57:47 +0100

    Add files via upload

--- assets/js/todos.js
+++ assets/js/todos.js
@@ -1,26 +1,51 @@
-// Check Off Specific Todos By Clicking
+// Checking off specific todos by clicking
+// $("li").click(function(){
+//     if($(this).css("color") === "rgb(128, 128, 128)"){
+//         $(this).css({
+//             color: "black",
+//             textDecoration: "none"
+//         });
+//     }
+//     else{
+//         $(this).css({
+//             color: "gray",
+//             textDecoration: "line-through"
+//         });
+//     }
+    
+
+// });
+
+// Checking off todos
+
 $("ul").on("click", "li", function(){
-	$(this).toggleClass("completed");
+$(this).toggleClass("completed");
 });
 
-//Click on X to delete Todo
+//click on X to delete a todo
+
 $("ul").on("click", "span", function(event){
-	$(this).parent().fadeOut(500,function(){
-		$(this).remove();
-	});
-	event.stopPropagation();
+    $(this).parent().slideToggle(200, function(){
+        $(this).remove();
+    });
+    event.stopPropagation();
 });
+
+//add a new todo
 
 $("input[type='text']").keypress(function(event){
-	if(event.which === 13){
-		//grabbing new todo text from input
-		var todoText = $(this).val();
-		$(this).val("");
-		//create a new li and add to ul
-		$("ul").append("<li><span><i class='fa fa-trash'></i></span> " + todoText + "</li>")
-	}
+    if(event.which === 13){
+        // grabbing text from text input
+       var todoText = $(this).val();
+       $(this).val("");
+       // creating new li and add to ul
+       $("ul").append("<li><span><i class='fa fa-trash-o' aria-hidden='true'></i></span>" + todoText + "</li>");
+    
+    };
 });
 
-$(".fa-plus").click(function(){
-	$("input[type='text']").fadeToggle();
+$("input[type='text']").hide();
+
+$(".fa-plus-circle").click(function(){
+    $("input[type='text']").slideToggle(200);
 });
